0cf1c53992cb57577092ffcecf4b4194eb4d67fd
Sebas Corral <sebascorralbrito@gmail.com>
2022-02-15 12:58:34 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
